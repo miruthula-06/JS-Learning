89b24a47c660cda40e3521e8c64a997bbda10047
miruthula <miruthuladeventhiran@gmail.com>
2026-09-08 01:34:35 +0530

    new file

---
+++ file1.js
@@ -0,0 +1,21 @@
+//Find positive and negative number
+// let a=10;
+// if(a<0){
+//     console.log("negative")
+// }else{
+//     console.log("positive")
+// }
+
+//Find even and odd number
+// let a=5;
+// if(a%2==0){
+//     console.log("even")
+// }else{
+//     console.log("odd")
+// }
+
+let username = "admin";
+let password = "1234"
+password="12"
+let  result=username === "ad" && password ? "login success":"Invalid login"
+console.log(result)
